8cdb6ebcb10a08784430ab84e3144be776da162f
rabbit-22 <chlwlal20@gmail.com>
2021-08-03 15:22:46 +0900

    7장-1 리덕스 설치 및 기초개념

---
+++ src/stories/ReduxStory.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import ReduxApp01 from '../07/ReduxApp01';
+
+storiesOf('ReduxApp', module).addWithJSX('기본 스토어 설정', () => <ReduxApp01 />);
