e70fb516d52d98cc5fb6f40306ae898c3844e374
rabbit-22 <chlwlal20@gmail.com>
2021-08-15 20:50:33 +0900

    12장-1,2 라우터 구성

--- src/08/CoinApp.jsx
+++ src/08/CoinApp.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { Provider } from 'react-redux';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AppLayout from './components/AppLayout';
 import CoinOverview from './components/main/CoinOverview';
 
@@ -8,19 +8,28 @@ import TransactionListContainer from './containers/main/TransactionListContainer
 import ModalProvider from './ModalProvider';
 import configureStore from './store/configureStore';
 import NotificationContainer from './containers/NotificationContainer';
+import MainPage from './components/main/MainPage';
+import NotFound from './components/NotFound';
 class CoinApp extends PureComponent {
   store = configureStore();
 
   render() {
     return (
       <Provider store={this.store}>
-        <ModalProvider>
-          <AppLayout>
-            <CoinOverview />
-            <TransactionListContainer />
-            <NotificationContainer />
-          </AppLayout>
-        </ModalProvider>
+        <Router>
+          <ModalProvider>
+            <AppLayout>
+              <Switch>
+                <Route path="/" exact render={() => <MainPage />} />
+                <Route
+                  path="*"
+                  component={NotFound} // match 프로퍼티로 입력한 주소값 전달하기 위해 컴포넌트 프로퍼티 사용
+                />
+              </Switch>
+              <NotificationContainer />
+            </AppLayout>
+          </ModalProvider>
+        </Router>
       </Provider>
     );
   }
